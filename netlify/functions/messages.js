1fe666ce5bcba2500e286159df29a620dfa07eb6
tdelispe <85971310+tdelispe@users.noreply.github.com>
2025-03-17 16:35:48 +0200

    Update messages.js

--- netlify/functions/messages.js
+++ netlify/functions/messages.js
@@ -2,6 +2,7 @@ let messages = []; // Ο πίνακας των μηνυμάτων
 
 exports.handler = async function (event, context) {
     if (event.httpMethod === 'GET') {
+        console.log('Αποστολή μηνυμάτων:', messages); // Προσθήκη log για να ελέγξουμε τι στέλνουμε
         return {
             statusCode: 200,
             body: JSON.stringify({ messages }) // Επιστρέφουμε τα μηνύματα
